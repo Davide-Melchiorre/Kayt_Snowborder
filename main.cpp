be66558f3a66c2eb6dd82e6bfdf0b60f9d89e604
Davide-Melchiorre <melchiorredavide4@gmail.com>
2023-02-26 18:56:28 +0100

    Programma c++ finito

--- main.cpp
+++ main.cpp
@@ -11,8 +11,9 @@ const int c=10;
 struct Atleta
 {
     string matricola,cognome;
-    int x[30],y[30];
-    float distanza;
+    int x[30];
+    int y[30];
+    float distanza=0;
 };
 
 void visualizzazione(Atleta snowborder[], int &i)
@@ -22,9 +23,11 @@ void visualizzazione(Atleta snowborder[], int &i)
     while(getline(fin,str, ';'))
     {
         snowborder[i].matricola=str;
-        cout<<str<<" ";
+        cout<<' '<<str<<"   ";
         getline(fin,snowborder[i].cognome);
         cout<<snowborder[i].cognome<<" ";
+        snowborder[i].y[0]=0;
+        snowborder[i].x[0]=0;
         i++;
         cout<<endl;
     }
@@ -33,50 +36,43 @@ void visualizzazione(Atleta snowborder[], int &i)
 
 void gestione(Atleta snowborder[], int &i)
 {
-    int x=0,y=0,x1=11,y1=0;
+    int a=0,y=0,y1=11;
     for(int h=0; h<i; h++)
     {
-        snowborder[h].x[0]=0;
-        snowborder[h].y[0]=0;
-        for(int k=1; k<=30; k++)
+        for(int k=1; k<30; k++)
         {
-                x=rand()%x1+y;
+                y=rand()%y1+a;
                 if(k%3==0)
-                    y=y+10;
+                    a=a+10;
             snowborder[h].x[k]=rand()%101;
-            snowborder[h].y[k]=x;
-
+            snowborder[h].y[k]=y;
         }
-        y=0;
+        a=0;
     }
-
-    for(int h=0; h<i; h++)
+   for(int h=0; h<i; h++)
     {
-        for(int k=1; k<=30; k++)
+        for(int k=1; k<30; k++)
         {
             snowborder[h].distanza+=sqrt((pow(snowborder[h].x[k]-snowborder[h].x[k-1],2))+(pow(snowborder[h].y[k]-snowborder[h].y[k-1],2)));
         }
     }
+
     cout<<endl<<"Coordinate generate correttamente!"<<endl<<endl;
 
-    fstream fout("atleti.csv");
+    fstream fout("gara.csv");
     for(int h=0; h<i; h++)
     {
         fout<<snowborder[h].matricola<<';';
         fout<<snowborder[h].cognome<<';';
-        for(int k=1; k<=30; k++)
+        for(int k=0; k<30; k++)
         {
-        fout<<snowborder[h].x[k]<<';';
-        fout<<snowborder[h].y[k]<<';';
+            fout<<snowborder[h].x[k]<<';';
+            fout<<snowborder[h].y[k]<<';';
         }
         fout<<snowborder[h].distanza<<endl;
     }
     fout.close();
 
-    /*for(int h=0; h<i; h++)
-    {
-        cout<<snowborder[h].matricola<<"  "<<snowborder[h].cognome<<"  "<<snowborder[h].distanza<<endl;
-    }*/
 }
 
 void risultati(Atleta snowborder[], int &i)
@@ -89,12 +85,43 @@ void risultati(Atleta snowborder[], int &i)
     cout<<endl;
 }
 
-void podio(Atleta snowborder[], int &i)
+void podio(Atleta snowborder[], int &i, int &scelta)
 {
     Atleta appoggio;
+    for(int a=0;a<i-1;a++)
+    {
+        for(int b=a+1;b<i;b++)
+        {
+            if(snowborder[b].distanza>snowborder[a].distanza)
+            {
+                appoggio=snowborder[b];
+                snowborder[b]=snowborder[a];
+                snowborder[a]=appoggio;
+            }
+        }
+    }
 
+    cout<<"          PODIO   "<<endl;
+    for(int c=0;c<3;c++)
+    {
+         cout<<"  "<<c+1<<"  "<<snowborder[c].cognome<<"  "<<snowborder[c].distanza<<endl;
+    }
+    string s;
+    cout<<endl<<"vuoi avviare una nuova gara? [si/no]"<<endl;
+    cin>>s;
+    if(s=="si"||s=="Si")
+    {
+        for(int a=0;a<i;a++)
+        {
+            snowborder[a].distanza=0;
+        }
+            i=0;
+    }
+    else
+    {
+        scelta=5;
+    }
 }
-
 
 int main()
 {
@@ -123,19 +150,11 @@ int main()
                         risultati(snowborder,i);
                         break;
                     case 4:
-                        podio(snowborder,i);
+                        cout<<endl;
+                        podio(snowborder,i,scelta);
+                        cout<<endl;
                         break;
                 }
     }while(scelta!=5);
     return 0;
 }
-/*1;D'Addetta
-2;Di vito
-3;De Bonis
-4;Saracino
-5;Martella
-6;Mischitelli
-7;Di Tullo
-8;Gentile
-9;Totaro
-10;Pompilio*/
